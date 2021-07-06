86121c997d43d50aa6c69f6c563ec1120e413823
Tran Long An <46746792+antran2123153@users.noreply.github.com>
2021-07-06 09:31:18 +0700

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://bds-web-server.herokuapp.com",
+  baseURL: "https://antran2123153.github.io/bds-web-server",
 });
 
 // instance.interceptors.request.use((config) => {
