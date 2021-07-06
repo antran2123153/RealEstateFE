9091de3db67e29c00f3a76f45a5fc47a9d275084
Long Ẩn Trần <an.tran2123153@hcmut.edu.vn>
2021-07-06 15:16:54 +0700

    axios url

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const instance = axios.create({
-  baseURL: "https://bds-web-server.herokuapp.com",
-});
+axios.defaults.baseURL = "https://bds-web-server.herokuapp.com";
 
 export default instance;
