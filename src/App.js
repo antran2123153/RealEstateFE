78d4d8359dfcf78bc3566f491755ed057945317a
Long Ẩn Trần <an.tran2123153@hcmut.edu.vn>
2021-07-13 16:36:47 +0700

    rename dir

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import User from "./user/User";
-import Admin from "./admin/Admin";
+import Admin from "./manager/Admin";
 import "./css/index.css";
 
 class App extends Component {
